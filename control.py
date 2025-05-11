8557190f2f1dd68016d363612687b9b5ae859419
Luc <lucrfd@gmail.com>
2025-05-10 23:56:37 -0400

    created methods to filter spells by class and school

--- control.py
+++ control.py
@@ -4,18 +4,43 @@ import helpers
 import cacher
 
 
-def display_spell(spell_name):
+def display_spell(spell):
+    spell.output()
 
-    print("spell name: ",spell_name)
 
-    helpers.search_spell(cacher.cache_search(helpers.reformat(spell_name)))
+def filter_by_class(list, filter_class):
+
+    filtered_spells = [spell for spell in list if filter_class in [s.lower() for s in spell.spell_lists]]
+
+    [print(spell.name) for spell in filtered_spells]
+    
+    return filtered_spells
+
+
+def filter_by_school(list, filter_class):
+
+    filtered_spells = [spell for spell in list if filter_class in [s.lower() for s in spell.spell_lists]]
+
+    [print(spell.name) for spell in filtered_spells]
+    return filtered_spells
 
 
 def main():
 
+    print('------Bookwyrm------')
+
+    spells = cacher.initialize_spells()
+
+    if(spells == None):
+        print("Aborting program.")
+        return
+
+
+
+
     user_input = input("Enter spell name: ")
 
-    display_spell(user_input)
+    filter_by_class(spells, user_input)
 
 
 if __name__ == "__main__":
